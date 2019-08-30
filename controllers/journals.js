6bacf5ea12d9bd62028a3f3c788ade967ba00409
Frances Cancio <frances.cancio@gmail.com>
2019-08-29 20:27:00 -0500

    Adds delete journal entry function

--- controllers/journals.js
+++ controllers/journals.js
@@ -1,8 +1,20 @@
 var Custom = require('../models/custom');
 
 module.exports = {
-    create
+    create,
+    delete: deleteJournal
 };
+
+function deleteJournal(req, res) {
+    console.log(req.params)
+    Custom.findById(req.params.id, (err, custom) => {
+      custom.journals.remove(req.params.journal)
+      custom.save(()=>{
+        res.redirect(`/entries/customs/${custom._id}`);
+      })
+      // console.log(req.params.id);
+    });
+}
 
 function create(req, res) {
     Custom.findById(req.params.id, function(err, custom) {
--- routes/journals.js
+++ routes/journals.js
@@ -3,5 +3,8 @@ var router = express.Router();
 var journalsCtrl = require('../controllers/journals');
 
 router.post('/entries/customs/:id/journals', journalsCtrl.create);
+router.delete('/entries/customs/:id/journals/:journal', journalsCtrl.delete);
+
+
 
 module.exports = router;
